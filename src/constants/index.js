ba1f9530af9133fcab98b79cca342231d5d0320d
Jesús Cautivo Chacón <cautivo.chacon@gmail.com>
2018-10-04 09:19:33 +0200

    Fix api endpoint for production

--- src/constants/index.js
+++ src/constants/index.js
@@ -2,6 +2,6 @@ import ACTIONS from "./actions";
 
 export const AUTH_TOKEN = "auth-token";
 
-export const API_URL = " https://0a663058.ngrok.io/api/v1";
+export const API_URL = "https://api.fudit.es/api/v1";
 
 export const ACTIONS_TYPES = ACTIONS;
